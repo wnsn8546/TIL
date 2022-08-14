e6e0ae0efff379931f3e053b1c37b189966c9588
wnsn8546 <wnsn8546@gmail.com>
2022-08-14 23:39:02 +0900

    modified

---
+++ Algorithm/SWExpertAcademy/D2/1945.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    N = input()
+    result = [0, 0, 0, 0, 0]
+    temp = [0, 0, 0, 0, 0,]
+    flag = 0
+    while flag == 0:
+
+
+    print(f'#{test_case} {result}')
