0bc77b32370c4a2c6f145d069c421e1cf22c9da8
wnsn8546 <wnsn8546@gmail.com>
2022-09-06 23:59:12 +0900

    0906

---
+++ Algorithm/Doit_Python/001.py
@@ -0,0 +1,5 @@
+# https://www.acmicpc.net/problem/11720
+
+N = int(input())
+nums = list(map(int, input()))
+print(sum(nums))
---
+++ Algorithm/Doit_Python/002.py
@@ -0,0 +1,8 @@
+# https://www.acmicpc.net/problem/1546
+
+N = int(input())
+scores = list(map(int, input().split()))
+M = max(scores)
+for i in range(len(scores)):
+    scores[i] = scores[i] / M * 100
+print(sum(scores) / N)
