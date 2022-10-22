be2c240e66df9b92bace0203f72146ede522bb03
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2022-10-23 00:19:21 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    printf("|\\_/|");
+    printf("\n");
+    printf("|q p|   /}");
+    printf("\n");
+    printf("( 0 )\"\"\"\\");
+    printf("\n");
+    printf("|\"^\"`    |");
+    printf("\n");
+    printf("||_/=\\\\__|");
+    return 0;
+}
