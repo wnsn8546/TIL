4d4e8fb546fad9c2b68f2f82964aa28e550901bf
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2022-11-09 23:53:57 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+  int n;
+  int i;
+  int a;
+  int b;
+  scanf("%d",&n);
+  
+
+for(i=0;i<n;i++)
+{
+    scanf("%d %d",&a,&b);
+    printf("%d\n",a+b);
+}
+    return 0;
+}
