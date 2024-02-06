5dc6fcb5f8ed4f4afc7620640cd41f1de161467a
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-02-06 18:01:55 +0900

    [level 0] Title: 문자열 뒤집기, Time: 1.40 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,10 +1,10 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        String[] splited =  my_string.split("");
+        String[] split =  my_string.split("");
         
-        for (int i = splited.length-1 ; i >= 0 ;i--) {
-            answer += splited[i];
+        for (int i = split.length-1 ; i >= 0 ;i--) {
+            answer += split[i];
         }
         
         return answer;
