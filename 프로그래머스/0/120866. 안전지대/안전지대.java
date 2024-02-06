13da8072467339dc57ca011ca62894b5d438e573
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-02-07 07:02:11 +0900

    [level 0] Title: 안전지대, Time: 0.24 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board) {
+        int answer = 0;
+        // board 에서 지뢰 1을 기준으로 위험방향인 8방향을 전부 -1로 바꿔주고 전체에서 1의 개수를 빼준다.
+        int [][] direction = {{0, 1},
+                              {1, 1},
+                              {1, 0},
+                              {1, -1},
+                              {0, -1},
+                              {-1, -1},
+                              {-1, 0},
+                              {-1,1}};
+        
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                if(board[i][j] == 1) {
+                    for(int k = 0; k < direction.length;k++) {
+                        if(i+direction[k][0] < board.length && i+direction[k][0] >= 0 &&
+                           j+direction[k][1] < board.length && j+direction[k][1] >= 0 && 
+                           board[i+direction[k][0]][j+direction[k][1]] != 1) {
+                            board[i+direction[k][0]][j+direction[k][1]] = -1;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(board));
+
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == 0) {
+                    answer += 1;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
