a2f1891016f9c4c032447c5be9c3b7abca9197a2
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-02-07 08:44:27 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -3,7 +3,8 @@ import java.util.*;
 class Solution {
     public int solution(int[][] board) {
         int answer = 0;
-        // board 에서 지뢰 1을 기준으로 위험방향인 8방향을 전부 -1로 바꿔주고 전체에서 1의 개수를 빼준다.
+        // board 에서 지뢰 1을 기준으로 위험방향인 8방향을 전부 -1로 바꿔주고 전체에서 0의 개수를 더해준다.
+        // 조건문으로 -1로 바꿔줄 인덱스가 해당하는곳이 배열 범위를 벗어나거나, 지뢰가 있던 자리이면 제외한다.
         int [][] direction = {{0, 1},
                               {1, 1},
                               {1, 0},
@@ -26,7 +27,6 @@ class Solution {
                 }
             }
         }
-        System.out.println(Arrays.deepToString(board));
 
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
