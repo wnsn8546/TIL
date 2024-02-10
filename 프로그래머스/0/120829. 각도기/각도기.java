b7ced0bed5066aaf5c18925bd5cb76c7ba46294f
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-02-10 17:45:43 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        
+        if(angle == 180) {
+            return 4;
+        } else if(angle < 180 && angle > 90){
+            return 3;
+        }else if(angle == 90){
+            return 2;
+        }else if(angle < 90 && angle > 0){
+            return 1;
+        }
+        
+        return answer;
+    }
+}
