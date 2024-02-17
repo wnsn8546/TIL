1504b8576c7209855307573abf8ce411b2bd48f5
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-02-17 12:18:43 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 0.07 ms
+메모리: 76 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 23:48:37
+2024년 2월 6일 12:21:4
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,9 +1,10 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        // 문자열을 해당 인덱스에 넣은 배열을 만들어주고,
         String[] arr = {"zero","one","two","three","four","five","six","seven","eight","nine"};
         
-        for(int i=0;i<arr.length;i++) {
+        for(int i=0;i<arr.length;i++) { // 배열에 들어 있는 문자열이 나오면 해당 인덱스로 교체해준다.
         	if(s.contains(arr[i])) {
         		s = s.replace(arr[i], Integer.toString(i));
         	}
