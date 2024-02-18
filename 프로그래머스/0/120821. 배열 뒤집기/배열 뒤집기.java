436cf2bcc5c7e234dac4673dd3385b42b08b07e8
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-02-18 15:28:08 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] solution(int[] num_list) {
+        // 원소를 거꾸로 출력하기
         int[] answer = new int[num_list.length];
         
         for(int i = num_list.length-1; i >= 0; i--) {
             answer[num_list.length-i-1] = num_list[i];
         }
-        
         
         return answer;
     }
