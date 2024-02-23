f4ec6255aa53d5f13848c84ec3dc5c7709747d0e
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-02-23 16:30:08 +0900

    [level 1] Title: 시저 암호, Time: 38.75 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(String s, int n) {
-        // 문자열 다 담아 놓고 인덱스 n씩 더해한 문자열로 리턴한다.
-        String[] sList = s.split("");
+        // 문자열 다 담아 놓고 인덱스 n씩 더한 문자열로 리턴한다.
+        String[] sList = s.split(""); // 쪼개서 문자열 배열로 만들어 둔다
         String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
         
         String answer = "";
-        for(String c : sList) {
+        for(String c : sList) { // 하나씩 순회하면서
             char cTemp = c.charAt(0);
             if(Character.isUpperCase(cTemp)) { // 대문자 일때
-                String temp = Character.toString(Character.toLowerCase(cTemp));
+                String temp = Character.toString(Character.toLowerCase(cTemp)); // 소문자로 바꿔고,
                 
-                for(int i = 0;i < alphabet.length ;i++) {
+                for(int i = 0;i < alphabet.length ;i++) { // 해당 문자를 찾아 대문자로 바꾼 뒤에answer에 넣어준다.
                     if(alphabet[i].equals(temp)) {
                         answer += alphabet[(i+n) % 26].toUpperCase(); 
                     }
                 }
-            } else if(c.equals(" ")) {
+            } else if(c.equals(" ")) { // 공백일때.
                 answer += " ";
             } else { // 소문자 일때
                 for(int i = 0;i < alphabet.length ;i++) {
