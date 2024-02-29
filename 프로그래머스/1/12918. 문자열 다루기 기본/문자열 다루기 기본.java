6d80e9484b880db91b6cb832c84893d496ccb3ba
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-02-29 23:48:23 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.11 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,6 +1,6 @@
 // 문자열 다루기 기본
 class Solution {
     public boolean solution(String s) {
-        return s.matches("[0-9]{4}|[0-9]{6}");
+        return s.matches("[0-9]{4}|[0-9]{6}"); // 몇일 전에 프로그래머스 책 보고 외움. 숫자로만4개 또는 숫자로만 6개이냐? false,true.
     }
 }
