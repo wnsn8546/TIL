f08390fddd0494f36bcd745f7f38a90332af65fc
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-03-04 03:07:37 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.12 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502#) 
 
 ### 성능 요약
 
-메모리: 74.1 MB, 시간: 0.12 ms
+메모리: 73 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 04일 03:09:56
+2024년 03월 04일 03:10:17
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,24 +1,21 @@
-import java.util.Stack;
+import java.util.*;
 // 햄버거 만들기
 class Solution {
     public int solution(int[] ingredient) {
         // 빵1 - 야채2 - 고기3 - 빵1
-        // 스택으로
-        // 1,2,3,1 이 나오면 해당 부분을 빼고 그 전 인덱스부터 다시.
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
         String hamburger = "1231";
+        String ingredientStr = Arrays.toString(ingredient).replaceAll("[^0-9]", "");
         
-        for(int i = 0; i < ingredient.length; i++) {
-            stack.push(ingredient[i]);
-            if(stack.size() >= 4) {
-                if(stack.get(stack.size()-4) == 1 && stack.get(stack.size()-3) == 2 && stack.get(stack.size()-2) == 3 && stack.get(stack.size()-1) == 1) {
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                    answer++;
-                }
+        while (true) {
+            int beforeLength = ingredientStr.length();
+            ingredientStr = ingredientStr.replaceFirst("1231", "");
+            int afterLength = ingredientStr.length();
+            
+            if(beforeLength == afterLength) {
+                break;
+            } else {
+                answer++;
             }
         }
 
