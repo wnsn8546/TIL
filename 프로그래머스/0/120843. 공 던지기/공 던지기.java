10ddff783394ac7a145dd517adec42603b3f91e2
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-03-06 20:27:26 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,10 @@
+// 공 던지기
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        
+        answer = numbers[(2 * (k-1)) % numbers.length];
+        
+        return answer;
+    }
+}
