0ebadafaf1d72abacab65b3d0ea888f4d9031aea
wnsn8546 <108647457+wnsn8546@users.noreply.github.com>
2024-05-23 03:01:51 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.47 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        
+        int[] answer = new int[num_list.length - 5];
+        
+        for(int i = 0; i < num_list.length - 5; i++) {
+            answer[i] = num_list[i+5];
+        }
+        
+        return answer;
+    }
+}
